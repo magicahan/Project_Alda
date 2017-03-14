81910ae0eb32e0d70fea008acaf6d593fa5ef47b
Dongping Zhang <dpzhang@berkeley.edu>
2017-03-13 21:19:31 -0500

    update shell scripting

--- alda_init.sh
+++ alda_init.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo python3 -m pip install Django==1.10.6
 sudo python3 -m pip install selenium
 sudo python3 -m pip install numpy
-sudo python3 -m pip install re
 sudo python3 -m pip install openpyxl
 cd AldaCourse/aldacourse/
 python3 manage.py runserver
+xdg-open http://127.0.0.1:8000/courses
