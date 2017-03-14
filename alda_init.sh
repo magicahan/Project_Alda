eecdd1acf24c750125969a95eaa11b514a989a30
Anne Rogers <amr@cs.uchicago.edu>
2017-03-13 21:47:35 -0500

    update shell scripting should be working now

--- alda_init.sh
+++ alda_init.sh
@@ -1,13 +1,3 @@
 #!/bin/bash  
- 
-# Bash script for installing most modules for Whiteboard.
 
-sudo apt-get clean
-sudo apt-get update
-sudo python3 -m pip install Django==1.10.6
-sudo python3 -m pip install selenium
-sudo python3 -m pip install numpy
-sudo python3 -m pip install openpyxl
-cd AldaCourse/aldacourse/
-python3 manage.py runserver
 xdg-open http://127.0.0.1:8000/courses
---
+++ alda_prep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+ 
+# Bash script for installing most modules for Whiteboard.
+
+sudo apt-get clean
+sudo apt-get update
+sudo python3 -m pip install Django==1.10.6
+sudo python3 -m pip install selenium
+sudo python3 -m pip install numpy
+sudo python3 -m pip install openpyxl
+cd AldaCourse/aldacourse/
+python3 manage.py runserver
